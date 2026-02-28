e62efd104aa9cb18f276a7103c4d1979c7e1face
Jeff Held <jh.software.developer@gmail.com>
2026-02-28 18:37:00 +0100

    Add footer, add some styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,14 +21,77 @@
 		spectrum.
 	</p>
 
-	{#if state.isAsawUpcomingThisYear || state.isItAsaw}
-		<p>ASAW runs this year from {dateFormat.formatRange(...state.asawDateRange)}</p>
-	{/if}
-
-	{#if state.didAsawOccurThisYear}
+	{#if state.isItAsaw}
+		<p>
+			ASAW is currently running this year from {dateFormat.formatRange(...state.asawDateRange)}
+		</p>
+	{:else if state.isAsawUpcomingThisYear}
+		<p>ASAW will run this year from {dateFormat.formatRange(...state.asawDateRange)}</p>
+	{:else}
 		<p>
 			ASAW ran this year {dateFormat.formatRange(...state.asawDateRange)}. Next year it will run
 			from {dateFormatWithYear.formatRange(...getAsawDateRange(state.currentYear + 1))}
 		</p>
 	{/if}
 </main>
+
+<footer>
+	<p>
+		Made with 💚🤍🩶🖤 by
+		<a href="https://github.com/solkaz" rel="_noopener" target="_blank" class="monospace">solkaz</a>
+	</p>
+</footer>
+
+<style>
+	:global(body, html) {
+		margin: 0;
+		padding: 0;
+	}
+	:global(*) {
+		box-sizing: border-box;
+	}
+
+	:global(body) {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	main {
+		flex: 1 1 auto;
+
+		max-height: 800px;
+		width: 80%;
+		margin: 2rem auto;
+		display: flex;
+		flex-flow: column nowrap;
+		align-items: center;
+		justify-content: center;
+	}
+
+	@media screen and (width >= 500px) {
+		main {
+			flex: 1 1 auto;
+
+			max-height: 800px;
+			width: 80%;
+			margin: 2rem auto;
+			border: 1px solid black;
+			display: flex;
+			flex-flow: column nowrap;
+			align-items: center;
+			justify-content: center;
+		}
+	}
+
+	footer {
+		background-color: #f1f1f1;
+		padding: 1rem;
+		text-align: center;
+	}
+
+	a.monospace {
+		font-family: monospace;
+	}
+</style>
